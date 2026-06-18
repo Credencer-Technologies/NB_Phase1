750b8c4f10e04cb24a4bf6faf573b3a6a1c45ee7
Umme-Ruman18 <ruman@credencer.in>
2026-06-18 19:46:52 +0530

    updated explore page UI and Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Explore from "./Pages/Explore";
 
 function App() {
   return (
     <>
       <Header />
+      <Explore />
 
       {/* Page Content */}
 
