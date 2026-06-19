c1179e31a54e9fa383b589b23f3258efab9036cc
sowmya <madanakari67@gmail.com>
2026-06-19 15:05:54 +0530

    Registration pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-
-
+import Home from "./Pages/Home";
+import Explore from "./Pages/Explore";
 import Register from "./Pages/Register";
-
 function App() {
   return (
     <BrowserRouter>
       <Header />
 
       <Routes>
-       
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
         <Route path="/register" element={<Register />} />
       </Routes>
 
